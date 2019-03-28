1860d9d4ee66be95dc5e8e3e27630fe19dc3dc48
ezio17 <xd_ezio@ukr.net>
2019-03-28 21:45:09 +0200

    add build

---
+++ build/precache-manifest.328c58655339542316dd60031a5a54d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59dbc36d5c9540de2554",
+    "url": "/news-api/build/static/css/main.d06fbd45.chunk.css"
+  },
+  {
+    "revision": "59dbc36d5c9540de2554",
+    "url": "/news-api/build/static/js/main.c1f653f7.chunk.js"
+  },
+  {
+    "revision": "de10af6c48ec72961ac2",
+    "url": "/news-api/build/static/js/runtime~main.1b0c02cb.js"
+  },
+  {
+    "revision": "b060006bdf7b49a90327",
+    "url": "/news-api/build/static/js/2.f3a08c96.chunk.js"
+  },
+  {
+    "revision": "bb9d6c55b7d7ad566a5b49282d713e11",
+    "url": "/news-api/build/index.html"
+  }
+];
